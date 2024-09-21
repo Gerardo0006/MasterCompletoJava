dba66f6905b00abe429e79aec2b9c1df7844ef61
Gerardo <gerardo.dev21@hotmail.com>
2024-09-20 22:25:07 -0600

    Ejercicio 'Etiquetas en las sentencias for y while'

---
+++ HolaMundo/src/SentenciaBucleEtiquetas.java
@@ -0,0 +1,27 @@
+public class SentenciaBucleEtiquetas {
+    public static void main(String[] args) {
+        //bucle1: Esta es una etiqueta de bucle
+
+        bucle1: for (int i=0; i<5; i++){
+            System.out.println();
+            for (int j=0; j<5; j++){
+                if (i == 2){
+                    continue bucle1; //Omite la iteración pero sólo en el primer for, el que tenemos etiquetado como bucle1
+                }
+                System.out.print("[i= " + i + ", j= " + j + "] ");
+            }
+        }
+
+        System.out.println("\n\n-------------------------------------------------");
+
+        etiqueta: for (int i=0; i<5; i++){
+            System.out.println();
+            for (int j=0; j<5; j++){
+                if (i == 2){
+                    break etiqueta; //Omite la iteración pero sólo en el primer for, el que tenemos etiquetado como bucle1
+                }
+                System.out.print("[i= " + i + ", j= " + j + "] ");
+            }
+        }
+    }
+}
