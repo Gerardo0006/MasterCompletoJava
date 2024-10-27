3258c95181abbeb66469e9d4665e2580b528c0ad
Gerardo <gerardo.dev21@hotmail.com>
2024-10-26 23:41:10 -0600

    Tarea #13 terminada

--- ImprimirArrayIntercalandoPosiciones/src/ImprmirArrayIntercalandoPosiciones.java
+++ ImprimirArrayIntercalandoPosiciones/src/ImprmirArrayIntercalandoPosiciones.java
@@ -4,25 +4,23 @@ public class ImprmirArrayIntercalandoPosiciones {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int[] array = new int[4];
+        int[] array = new int[10];
         System.out.println("Ingresa un 10 números enteros para llenar el array:");
 
         //Llenamos el array
-        for (int i = 0; i<4; i++){
+        for (int i = 0; i<10; i++){
             System.out.print("Ingresa valor para array[" + i + "] : ");
             array[i] = scanner.nextInt();
         }
 
         //Algoritmo para acomodar el arreglo con el orden solicitado (último-primero, penúltimo-segundo, etc)
-        for (int i = 0; i<4; i++){
-            System.out.print("[" + array[array.length-i] + "]");
+        for (int i = 0; i < 5; i++){
+            System.out.print("[" + array[array.length-1-i] + "]");
             System.out.print("[" + array[i] + "]");
         }
-
-        //Imprimimos el array
-//        System.out.println();
-//        for (int i = 0; i<4; i++){
-//            System.out.print("[" + array[i] + "]");
+//        for (int i = 0; i < 5; i++) {
+//            System.out.print(array[9 - i] + " ");  // Último, penúltimo, antepenúltimo
+//            System.out.print(array[i] + " ");      // Primero, segundo, tercero
 //        }
     }
 }
