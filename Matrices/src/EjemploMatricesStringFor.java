8da954945a9fbd658fc1691517481f28740c3d3e
Gerardo <gerardo.dev21@hotmail.com>
2024-11-02 22:31:24 -0600

    Declaración/Definición de la matriz de tipo String

--- Matrices/src/EjemploMatricesStringFor.java
+++ Matrices/src/EjemploMatricesStringFor.java
@@ -1,5 +1,14 @@
 public class EjemploMatricesStringFor {
     public static void main(String[] args) {
+
+        //Declaramos el array
+        String[][] nombres = new String[3][2];
+        nombres[0][0] = "Pepe";
+        nombres[0][1] = "Pepa";
+        nombres[1][0] = "Josefa";
+        nombres[1][1] = "Paco";
+        nombres[2][0] = "Lucas";
+        nombres[2][1] = "Pancha";
         
     }
 }
