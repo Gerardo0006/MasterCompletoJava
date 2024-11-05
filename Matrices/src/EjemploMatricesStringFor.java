d6a7e44c82515fe81b5ecca8efd8e693559f8d47
Gerardo <gerardo.dev21@hotmail.com>
2024-11-04 22:05:41 -0600

    Ejercicio terminado. Se imprime la matriz dos veces, una usando bucles for anidados y otra usando bucles foreach anidados

--- Matrices/src/EjemploMatricesStringFor.java
+++ Matrices/src/EjemploMatricesStringFor.java
@@ -1,7 +1,7 @@
 public class EjemploMatricesStringFor {
     public static void main(String[] args) {
 
-        //Declaramos el array
+        //Declaramos la matriz
         String[][] nombres = new String[3][2];
         nombres[0][0] = "Pepe";
         nombres[0][1] = "Pepa";
@@ -9,5 +9,25 @@ public class EjemploMatricesStringFor {
         nombres[1][1] = "Paco";
         nombres[2][0] = "Lucas";
         nombres[2][1] = "Joel";
+
+        System.out.println("Iterando con un For:");
+        //El primer for es para las filas
+        for (int i=0; i<nombres.length; i++){
+            //El segundo for es para las columnas
+            for (int j=0; j<nombres[i].length; j++){
+                System.out.print(nombres[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nIterando con un ForEach:");
+        //El primer foreach es para las filas
+        for (String[] fila : nombres){
+            //El segundo foreach es para las columnas
+            for (String nombre : fila){
+                System.out.print(nombre + "\t");
+            }
+            System.out.println();
+        }
     }
 }
