b536add1898e3798ced2e2e3f7d930a6d2b2ea0f
Gerardo <gerardo.dev21@hotmail.com>
2024-12-27 22:34:47 -0600

    Moví los archivos que llevaba al momento de la sección 'poo' a una carpeta para comenzar desde cero la sección y entender los ejercicios de mejor manera.

---
+++ poo/src/poo-codigoConComentarios/Automovil.java
@@ -0,0 +1,250 @@
+//Las clases tienen 2 modificadores de acceso (public y default (para usar
+// default no se escribe nada antes del nombre de la clase)).
+public class Automovil {
+
+    //Declaramos los atributos
+    //Una buena práctica es que los atributos siempre deben ser
+    // privados para acceder a ellos solamente desde la clase
+    // donde se definen los métodos. A continuación pongo privados todos los atributos con private.
+    private String fabricante;
+    private String modelo;
+    //private String color = "gris";
+    //Cambiamos el tipo de dato de color que es String, por el tipo enum que es Color
+    private Color color = Color.GRIS;
+    private double cilindrada;
+    //Agregamos nuevo atributo/característica
+    private int capacidadTanque = 40;
+    //Agregamos ID para cada instancia/objeto
+    private int id;
+
+    //Agregamos atributo para poder hacer uso de enum TipoAutomovil
+    private TipoAutomovil tipo;
+
+    //Agregamos atributo estático
+//    private static String colorPatente = "Naranja";
+    //Cambiamos el tipo de dato de color que es String, por el tipo enum que es Color
+    private static Color colorPatente = Color.NARANJA;
+    private static int capacidadEstanqueEstatico = 30;
+    //Agregamos atributo estático ID para cada instancia/objeto
+    private static int ultimoId;
+
+    //Agregamos un valor constante, el cuál identificamos con la palabra reservada 'final'
+    //Las constantes se declaran con mayúsculas y palabras compuestas separadas con guión bajo
+    public static final Integer VELOCIDAD_MAX_CARRETERA = 120;
+    public static final Integer VELOCIDAD_MAXIMA_CIUDAD = 60;
+
+    //Agregamos nueva constatnte
+    public static final String COLOR_ROJO = "Rojo";
+    public static final String COLOR_AMARILLO = "Amarillo";
+    public static final String COLOR_AZUL = "Azul";
+    public static final String COLOR_BLANCO = "Blanco";
+    public static final String COLOR_GRIS = "Gris";
+
+    //Implementamos un Método vacío
+    public Automovil(){
+        /*Cada que se crea el objeto en éste método constructor
+          incrementamos ultimoId que es estático y se lo asignamos
+          al atributo id de la instancia.*/
+        this.id = ++ultimoId;
+    }
+
+    //Implementamos el Método Constructor
+    public Automovil(String fabricante, String modelo){
+        //Invocamos al constructor sin parámetros para que se asigne el id
+        this();
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+    }
+
+    //Implementamos Método constructor con tres parámetros
+    public Automovil(String fabricante, String modelo, /*Cambiamos el tipo string por tipo enum (Color). String*/Color color) {
+        //Vamos a optimzar el código haciéndo uso de this. Vamos a llamar al constructor anterior.
+        /*this.fabricante = fabricante;
+        this.modelo = modelo;*/
+        this(fabricante, modelo);
+        this.color = color;
+    }
+
+    //Implementamos Método constructor con cuatro parámetros
+    public Automovil(String fabricante, String modelo, /*Cambiamos el tipo string por tipo enum (Color). String*/Color color, double cilindrada) {
+        /*this.fabricante = fabricante;
+        this.modelo = modelo;
+        this.color = color;*/
+        this(fabricante, modelo, color);
+        this.cilindrada = cilindrada;
+    }
+
+    //Implementamos Método constructor con cinco parámetros
+    public Automovil(String fabricante, String modelo, /*Cambiamos el tipo string por tipo enum (Color). String*/Color color, double cilindrada, int capacidadTanque) {
+        /*this.fabricante = fabricante;
+        this.modelo = modelo;
+        this.color = color;
+        this.cilindrada = cilindrada;*/
+        this(fabricante, modelo, color, cilindrada);
+        this.capacidadTanque = capacidadTanque;
+    }
+
+    /*Si no tenemos definidos los métodos SET y GET podemos agregarlos con
+    * click derecho, generate y selleccionando la opción getter y setter.*/
+
+    //Agregamos un nuevo Método GET para cada atributo
+    //Cambiamos el nombre de los métodos leer por get
+    public String getFabricante(){
+        return this.fabricante;
+    }
+    public String getModelo(){
+        return this.modelo;
+    }
+    public /*Cambiamos el tipo string por tipo enum (Color). String*/Color getColor(){
+        return this.color;
+    }
+    public double getCilindrada(){
+        return cilindrada;
+    }
+    public int getCapacidadTanque(){
+        return capacidadTanque;
+    }
+
+    //Agregamos un nuevo Método SET para cada atributo
+    //Este tipo de métodos no retornan nada (son tipo VOID).
+    //Cambiamos el nombre de los métodos asignar por set
+    public void setFabricante(String fabricante){
+        this.fabricante = fabricante;
+    }
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+    public void setColor(/*Cambiamos el tipo string por tipo enum (Color). String*/Color color){
+        this.color = color;
+    }
+    public void asignarCapacidadTanque(int capacidadTanque){
+        this.capacidadTanque = capacidadTanque;
+    }
+    public void setCilindrada(double cilindrada){
+        this.cilindrada = cilindrada;
+    }
+
+    /*
+    //Agregamos Métodos
+    public void detalle(){
+        //Es buena práctica y más recomendado usar this
+        //Es mala practica imprimir dentro del método con sout
+        String modelo = "Otro modelo";
+        System.out.println("auto.fabricante = " + fabricante); //Aquí aunque quitemos el
+        // this, hace referencia al atributo porque dentro del método no hay otra variable
+        // con el mismo nombre del método
+        System.out.println("modelo = " + modelo); //Se puede omitir la palabra this,
+        // pero en este caso está haciendo referencia al String modelo y no al atributo.
+        System.out.println("auto.modelo = " + this.modelo);
+        System.out.println("auto.color = " + this.color);
+        System.out.println("auto.cilindrada = " + this.cilindrada);
+    }*/
+
+
+    public static /*Cambiamos el tipo string por tipo enum (Color). String*/Color getColorPatente(){
+        return colorPatente;
+    }
+    public static void setColorPatente(/*Cambiamos el tipo string por tipo enum (Color). String*/Color colorPatente){
+        Automovil.colorPatente = colorPatente; //Lo diferenciamos con la clase Automovil.
+    }
+
+    //Agregamos métodos getter & setter para capacidadEstanqueEstatico
+    public static int getCapacidadEstanqueEstatico() {
+        return capacidadEstanqueEstatico;
+    }
+    public static void setCapacidadEstanqueEstatico(int capacidadEstanqueEstatico) {
+        Automovil.capacidadEstanqueEstatico = capacidadEstanqueEstatico;
+    }
+
+    //Agregamos métodos getter & setter para id
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    //Agregamos métodos getter & setter para TipoAutomovil
+    public TipoAutomovil getTipo() {
+        return tipo;
+    }
+    public void setTipo(TipoAutomovil tipo) {
+        this.tipo = tipo;
+    }
+
+    public String verDetalle(){
+        //StringBuilder sb = new StringBuilder(); //Optimización de Código, aquí no usamos la variable sb y por ende podemos omitirla
+        return ("auto.id = " + this.id +
+                "\nauto.fabricante = " + this.fabricante +
+                "\nauto.modelo = " + this.modelo  +
+                "\nauto.tipo = " + this.getTipo().getNombre() +
+                "\nauto.color = " + this.color/*Llamamos al método constructor que modifica el valor del color del enum.getColor()*/ +
+                "\nauto.patenteColor = " + /*Automovil.colorPatente*/ colorPatente/*Llamamos al método constructor que modifica el valor del color del enum.getColor()*/ + //Invocamos al valor del atributo sólo con el nombre del atributo, sin this o con el nombre de la clase seguido de "." y el nombre del atributo.
+                "\nauto.cilindrada = " + this.cilindrada);
+        //return sb.toString(); //Optimización de Código, aquí no usamos la variable sb y por ende podemos omitirla
+    }
+
+    //Otro método (acelerar)
+    public String acelerar(int rpm){
+        return "El auto " + this.fabricante + " está acelerando a " + rpm + "rpm";
+    }
+
+    //Otro método (frenar)
+    public String frenar(){
+        return this.fabricante + " " + this.modelo + " frenando!";
+    }
+
+    //Otro método (acelerar y frenar)
+    public String acelerarFrenar(int rpm){
+        String acelerar = acelerar(rpm);
+        String frenar = frenar();
+        return acelerar + "\n" + frenar;
+    }
+
+    //Nuevo método
+    public float calcularConsumo(int km, float porcentajeGasolina){
+        return km/(capacidadTanque*porcentajeGasolina);
+    }
+    //La sobre carga de métodos es crear dos métodos con el mismo nombre pero distintos parámetros/argumentos
+    public float calcularConsumo(int km, int porcentajeGasolina){
+        return km/(capacidadTanque*(porcentajeGasolina/100f));
+    }
+
+    //Implementamos método calcular consumo con atributo capacidadEstanqueEstático
+    public static float calcularConsumoEstatico(int km, int porcentajeGasolina){
+        return km/(capacidadEstanqueEstatico*(porcentajeGasolina/100f));
+    }
+
+    //Agregamos la sobreescritura de un método
+    @Override
+    public boolean equals(Object obj) {
+        //Agregué validación para verificar que se trata del mismo objeto ya que se compara por referencia
+        if (this == obj){
+            return true;
+        }
+
+        //Agregué validación para que sólo se pueda comparar con objetos de la misma clase
+        if (!(obj instanceof Automovil)){
+            return false;
+        }
+
+        Automovil a = (Automovil) obj;
+        //Agregué validación para que no acepté o no se compare con objetos vacíos
+        return (this.fabricante != null && this.modelo != null
+                && this.fabricante.equals(a.getFabricante())
+                && this.modelo.equals(a.getModelo()));
+    }
+
+    //Sobreescritura del Método toString
+    @Override
+    public String toString() {
+        return /*"Automovil{" +
+                "fabricante='" + fabricante + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", color='" + color + '\'' +
+                ", cilindrada=" + cilindrada +
+                ", capacidadTanque=" + capacidadTanque +
+                '}'; */
+        this.id + " : " + fabricante + " " + modelo;
+    }
+}
---
+++ poo/src/poo-codigoConComentarios/EjemploAutomovil.java
@@ -0,0 +1,119 @@
+import java.util.Date;
+
+public class EjemploAutomovil {
+    public static void main(String[] args) {
+        /*
+        //Instanciamos objeto
+        Automovil subaru = new Automovil();
+
+        //Setteamos los valores de los atributos
+        subaru.fabricante = "Subaru";
+        subaru.modelo = "Impreza";
+        subaru.color = "Azúl metálico";
+        subaru.cilindrada = 2.0;
+
+        //Imprimimos los valores del objeto subaru llamando al Método detalle
+        subaru.detalle(); //Nombre del objeto seguido de punto, seguido de detalle
+
+        System.out.println();
+
+        //Creamos otro objeto
+        Automovil mazda = new Automovil();
+        
+        mazda.fabricante = "Mazda";
+        mazda.modelo = "Mazda 3 Sedán";
+//        mazda.color = "Cereza"; //Comentado para que tome el valor por defecto (gris)
+        mazda.cilindrada = 2.0;
+
+        //Imprimimos los valores del objeto subaru llamando al Método detalle
+        mazda.detalle();
+
+         */
+
+        //Instanciamos objetos
+        //Le pasamos los valores de los atributos fabricante y modelo
+        Automovil subaru = new Automovil("Subaru", "Impreza");
+        Automovil mazda = new Automovil("Impreza", "Mazda 3 Hatchback", /*Le pasamos el valor a través de la constante enum*/ Color.GRIS /*"Gris Rata"*/, 3.0);
+        //Instanciamos un nuevo Objeto Automovil (vacío y después le pasamos por argumento el valor de los atributos)
+        Automovil nissan = new Automovil("Nissan", "370z", /*Le pasamos el valor a través de la constante enum*/ Color.ROJO /*"rojo"*/, 3.0, 40);
+        Automovil nissan2 = new Automovil("Nissan", "370z", /*Le pasamos el valor a través de la constante enum*/ Color.ROJO /*"rojo"*/, 3.0, 40);
+        //Creamos un nuevo objeto vacío para ejemplo
+        Automovil auto = new Automovil();
+
+        //Instanciamos objeto fecha
+        Date fecha = new Date();
+
+        /*//Definimos valores para los atributos de los objetos
+        subaru.fabricante = "Subaru";
+        subaru.modelo = "Impreza";
+        subaru.color = "Azúl metálico";
+        subaru.cilindrada = 2.0;
+
+        mazda.fabricante = "Mazda";
+        mazda.modelo = "Mazda 3 Sedán";
+        mazda.color = "Cereza"; //Si lo comentamos para que tome el valor por defecto (gris)
+        mazda.cilindrada = 2.0;*/
+
+        //Definimos los atributos de cada objeto a través de sus métodos SET
+        //Actualizamos el nombre de los métodos (se cambió asignar por set)
+        //subaru.setFabricante("Subaru"); //Comentamos la asignación de valor a los atributos a través de los métodos
+        //subaru.setModelo("Impreza"); //Comentamos la asignación de valor a los atributos a través de los métodos
+        subaru.setColor(/*Le pasamos el valor a través de la constante enum*/ Color.AZUL/*"Azúl Metálico"*/);
+        subaru.setCilindrada(3.0);
+
+        //mazda.setFabricante("Mazda"); //Comentamos la asignación de valor a los atributos a través de los métodos
+        //mazda.setModelo("Mazda 3 Hatchback"); //Comentamos la asignación de valor a los atributos a través de los métodos
+        //Comentaré el valor de los atributos que asignamos con el método set ya que los pasaremos por parámetro
+        /*mazda.setColor("Gris Rata");
+        mazda.setCilindrada(3.0);*/
+
+        //Imprimimos los valores de los objetos
+        System.out.println(subaru.verDetalle()); //Nombre del objeto seguido de punto, seguido de detalle
+        System.out.println();
+        System.out.println(mazda.verDetalle());
+        System.out.println();
+        System.out.println(nissan.verDetalle());
+        System.out.println();
+        //Verificamos si los objetos Nissan y Nissan2 son iguales
+        System.out.println("¿Son iguales? " + (nissan == nissan2));
+        System.out.println("¿Son iguales? (con método equals) " + nissan.equals(nissan2));
+
+        //Comparamos objetos (objeto 'auto' con objeto 'fecha')
+        System.out.println("¿Objeto 'auto' igual a objeto 'fecha'? " + auto.equals(fecha));
+        //Imprimimos objeto Nissan
+        //Todos los objetos son únicos y tienen un hashcode con el que se identifica
+        //En seguida imprimo el hashcode del objeto de manera implícita
+        System.out.println("Clase + @ + hashcode/ID -> " + nissan); //Si imprimimos solamente el objeto (sin métodos), se imprimirá su hashcode
+        //En seguida imprimo el hashcode del objeto de manera explícita
+        System.out.println("Manera explícita de Clase + @ + haschode/ID -> " + nissan.toString());
+
+        //Aquí imprimo el objeto 'nissan' con el método toString sobreescrito
+        System.out.println("Manera implícita " + nissan);
+        System.out.println("Manera explícita " + nissan.toString());
+
+        //Agregamos comparación de auto con nissan
+        /*Un objeto Nulo (sin referencias) NO puede invocar métodos*/
+        System.out.println(auto.equals(nissan));
+
+        System.out.println("Método Acelerar");
+        System.out.println(subaru.acelerar(3000));
+        System.out.println("Método Frenar");
+        System.out.println(subaru.frenar());
+        System.out.println("Método Acelerar");
+        System.out.println(mazda.acelerar(3500));
+        System.out.println("Método Frenar");
+        System.out.println(mazda.frenar());
+        System.out.println();
+        System.out.println("----------------------------");
+        System.out.println("Método acelerarFrenar");
+        System.out.println(subaru.acelerarFrenar(3000));
+        System.out.println();
+        System.out.println(mazda.acelerarFrenar(3000));
+        System.out.println();
+
+        //Imprimimos valores del nuevo método
+        System.out.println("Kilómetros por litro: " + subaru.calcularConsumo(300, 0.5f));
+        //Imprimimos valores con la sobrecarga de métodos
+        System.out.println("Kilómetros por litro: " + subaru.calcularConsumo(300, 60));
+    }
+}
