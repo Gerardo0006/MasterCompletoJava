e27606d7daf4066e7df5d592b333c17f063cd0ca
Gerardo <gerardo.dev21@hotmail.com>
2025-02-12 15:06:05 -0600

    Obteniendo la clase padre de una clase en particular. Herencia y el API de reflexión de Java.

--- poo_herencia/src/com/gerardodev/pooherencia/EjemploHerencia.java
+++ poo_herencia/src/com/gerardodev/pooherencia/EjemploHerencia.java
@@ -16,10 +16,10 @@ public class EjemploHerencia {
         profesor.setApellido("Guzmán");
         profesor.setMateria("Programación con lenguaje Java"); //Método propio de la clase Profesor
 
-        alumnoInter.setNombre("Peter");
-        alumnoInter.setApellido("Parker");
-        alumnoInter.setEscuela("UAA");
-        alumnoInter.setPais("Noruega");
+        alumnoInter.setNombre("Peter"); //Método propio de la clase Persona
+        alumnoInter.setApellido("Parker"); //Método propio de la clase Persona
+        alumnoInter.setEscuela("UAA"); //Método propio de la clase Alumno
+        alumnoInter.setPais("Noruega"); //Método propio de la clase AlumnoInternacional
 
         //Accedemos a nombre y apellido con los métodos get heredados de la clase padre Persona
         System.out.println("ALUMNO\n" + alumno.getNombre() + " " + alumno.getApellido() +
@@ -28,5 +28,16 @@ public class EjemploHerencia {
                 "\nMateria: " + profesor.getMateria());
         System.out.println("ALUMNO INTERNACIONAL\n" + alumnoInter.getNombre() + " " + alumnoInter.getApellido() +
                 "\nEscuela: " + alumnoInter.getEscuela() + "\nPaís: " + alumnoInter.getPais());
+
+        Class clase = alumnoInter.getClass();
+        System.out.println("----------------------");
+        //Con bucle while recorremos la jerarquía de clases. Que nos de el nombre de la clase y su clase padre
+        while (clase.getSuperclass() != null){
+            String claseHija = clase.getName();
+            String clasePadre = clase.getSuperclass().getName(); //Con getSuperclass() accedemos a la clase padre y con getName() obtenemos su nombre
+            System.out.println(claseHija + " es clase hija de clase padre " + clasePadre);
+            clase = clase.getSuperclass();
+        }
+        System.out.println("----------------------");
     }
 }
