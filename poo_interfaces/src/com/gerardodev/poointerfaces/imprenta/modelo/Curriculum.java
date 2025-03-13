2bec0bfdf0fc196a8859983cf6a3e2bec9bfcee0
Gerardo <gerardo.dev21@hotmail.com>
2025-03-12 20:52:50 -0600

    Añadiendo nueva clase concreta (clase Curriculum) a la jerarquía.

---
+++ poo_interfaces/src/com/gerardodev/poointerfaces/imprenta/modelo/Curriculum.java
@@ -0,0 +1,41 @@
+package com.gerardodev.poointerfaces.imprenta.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Curriculum extends Hoja{
+    //Implementación de atributos propios de la clase
+    private String persona;
+    private String carrera;
+    private List<String> experiencias;
+
+    //Implementación de Método Constructor
+    public Curriculum(String persona, String carrera, String contenido){
+        super(contenido);
+        this.persona = persona;
+        this.carrera = carrera;
+        this.experiencias = new ArrayList<>(); //Inicializamos la lista
+    }
+
+    //Implementamos el método add para agregar elementos a la lista 'experiencias'
+    public Curriculum addExperiencia(String exp){
+        experiencias.add(exp);
+        return this;
+    }
+
+    //Implementación de Método abstracto de la clase abstracta padre Hoja
+    @Override
+    public String imprimir() {
+        StringBuilder sb = new StringBuilder("Nombre: ");
+        sb.append(persona).append("\n")
+                .append("Resumen: ").append(this.contenido)
+                .append("\n").append("Profesión: ")
+                .append(this.carrera).append("\n")
+                .append("Experiencias: \n");
+        //Implementación de foreach para iterar en cada elemento de la lista 'experiencias'
+        for (String exp : experiencias){
+            sb.append("- ").append(exp).append("\n");
+        }
+        return sb.toString();
+    }
+}
